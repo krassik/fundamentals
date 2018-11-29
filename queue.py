23c19f641ad1f8c91da5a333c220dc9ef0ec9bc2
Krassi <krassimir.krustev@gmail.com>
2018-11-29 12:00:18 -0800

    Impl Queue: enq pop peek

--- queue.py
+++ queue.py
@@ -9,13 +9,13 @@ class Queue:
         self.storage = [head]
 
     def enqueue(self, new_element):
-        pass
+        self.storage.append(new_element)
 
     def peek(self):
-        pass 
+        return self.storage[0]
 
     def dequeue(self):
-        pass
+        return self.storage.pop(0)
     
 # Setup
 q = Queue(1)
@@ -24,20 +24,20 @@ q.enqueue(3)
 
 # Test peek
 # Should be 1
-print q.peek()
+print(q.peek())
 
 # Test dequeue
 # Should be 1
-print q.dequeue()
+print (q.dequeue())
 
 # Test enqueue
 q.enqueue(4)
 # Should be 2
-print q.dequeue()
+print( q.dequeue() )
 # Should be 3
-print q.dequeue()
+print( q.dequeue() )
 # Should be 4
-print q.dequeue()
+print( q.dequeue() )
 q.enqueue(5)
 # Should be 5
-print q.peek()
+print( q.peek() )
